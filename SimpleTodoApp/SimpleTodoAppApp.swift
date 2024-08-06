83f46f6005659ccf52569bc3b4695fcd624aad55
Mehmet <mehmetext68@gmail.com>
2024-08-06 14:45:17 +0300

    Initial Commit

---
+++ SimpleTodoApp/SimpleTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoAppApp.swift
+//  SimpleTodoApp
+//
+//  Created by Mehmet Konukçu on 6.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
