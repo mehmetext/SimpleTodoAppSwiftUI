7837c88c067ac1daf86f76e2453b6c0bc67ff519
Mehmet <mehmetext68@gmail.com>
2024-08-06 19:35:52 +0300

    Firebase configure added

--- SimpleTodoApp/Others/SimpleTodoAppApp.swift
+++ SimpleTodoApp/Others/SimpleTodoAppApp.swift
@@ -5,10 +5,15 @@
 //  Created by Mehmet Konukçu on 6.08.2024.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct SimpleTodoAppApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainView()
